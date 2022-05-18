a2f9012fea01f61ed587b202b30eb17d98866fcb
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-05-18 10:38:36 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネントです。
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+
+    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+</div>
